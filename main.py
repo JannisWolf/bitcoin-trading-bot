2656c221548c7d9d671290b996806c26362dcfb1
Jannis Wolf <jannis.wolf@fau.de>
2021-08-05 19:49:09 +0200

    integration of crypto data class

--- main.py
+++ main.py
@@ -2,15 +2,9 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import datetime
+from crypto_data import CryptoData
 
-from pycoingecko import CoinGeckoAPI
 
 if __name__ == "__main__":
-    cg = CoinGeckoAPI()
-    a7 = cg.get_coin_market_chart_by_id(id='bitcoin', vs_currency='usd', days='90')
-    p7 = np.array(a7['prices'])[:, 0]
-    b7 = np.array(a7['prices'])[:, 1]
-    p7d = [datetime.datetime.fromtimestamp(item / 1000) for item in p7]
-    plt.plot_date(p7d, b7, '-')
-    plt.show()
-    print('hello world')
+    data = CryptoData()
+    data.get_data(plot=True)
